755bf7e4723c1bbd5aa6b74bad8a1cf5e16a3f08
Guo Dong <guo.dong@intel.com>
2021-04-07 13:27:52 -0700

    Remove hard-coded resource reserve
    After remove, same payload could work on QEMU and TGL.
    
    Signed-off-by: Guo Dong <guo.dong@intel.com>

--- UefiPayloadPkg/BlSupportDxe/BlSupportDxe.c
+++ UefiPayloadPkg/BlSupportDxe/BlSupportDxe.c
@@ -7,76 +7,7 @@
 **/
 #include "BlSupportDxe.h"
 
-/**
-  Reserve MMIO/IO resource in GCD
 
-  @param  IsMMIO        Flag of whether it is mmio resource or io resource.
-  @param  GcdType       Type of the space.
-  @param  BaseAddress   Base address of the space.
-  @param  Length        Length of the space.
-  @param  Alignment     Align with 2^Alignment
-  @param  ImageHandle   Handle for the image of this driver.
-
-  @retval EFI_SUCCESS   Reserve successful
-**/
-EFI_STATUS
-ReserveResourceInGcd (
-  IN BOOLEAN               IsMMIO,
-  IN UINTN                 GcdType,
-  IN EFI_PHYSICAL_ADDRESS  BaseAddress,
-  IN UINT64                Length,
-  IN UINTN                 Alignment,
-  IN EFI_HANDLE            ImageHandle
-  )
-{
-  EFI_STATUS               Status;
-
-  if (IsMMIO) {
-    Status = gDS->AddMemorySpace (
-                    GcdType,
-                    BaseAddress,
-                    Length,
-                    EFI_MEMORY_UC
-                    );
-    if (EFI_ERROR (Status)) {
-      DEBUG ((
-        DEBUG_ERROR,
-        "Failed to add memory space :0x%lx 0x%lx\n",
-        BaseAddress,
-        Length
-        ));
-    }
-    ASSERT_EFI_ERROR (Status);
-    Status = gDS->AllocateMemorySpace (
-                    EfiGcdAllocateAddress,
-                    GcdType,
-                    Alignment,
-                    Length,
-                    &BaseAddress,
-                    ImageHandle,
-                    NULL
-                    );
-    ASSERT_EFI_ERROR (Status);
-  } else {
-    Status = gDS->AddIoSpace (
-                    GcdType,
-                    BaseAddress,
-                    Length
-                    );
-    ASSERT_EFI_ERROR (Status);
-    Status = gDS->AllocateIoSpace (
-                    EfiGcdAllocateAddress,
-                    GcdType,
-                    Alignment,
-                    Length,
-                    &BaseAddress,
-                    ImageHandle,
-                    NULL
-                    );
-    ASSERT_EFI_ERROR (Status);
-  }
-  return Status;
-}
 
 
 /**
@@ -249,14 +180,6 @@ BlDxeEntryPoint (
   SMBIOS_TABLE_HOB           *SmbiosTable;
 
   Status = EFI_SUCCESS;
-  //
-  // Report MMIO/IO Resources
-  //
-  Status = ReserveResourceInGcd (TRUE, EfiGcdMemoryTypeMemoryMappedIo, 0xFEC00000, SIZE_4KB, 0, ImageHandle); // IOAPIC
-  ASSERT_EFI_ERROR (Status);
-
-  Status = ReserveResourceInGcd (TRUE, EfiGcdMemoryTypeMemoryMappedIo, 0xFED00000, SIZE_1KB, 0, ImageHandle); // HPET
-  ASSERT_EFI_ERROR (Status);
 
   //
   // Install Acpi Table
