0a32bfed7e3bc8321d51748b38e0f80a750e15a4
thiyagukb <thiyagux.kesavan.balakrishnan@intel.com>
2021-09-16 08:04:30 +0530

    OvmfPkg: Add QemuPayloadPcdListLibNull to list PCDs used by Platform.
    Library instance to list all DynamicEx PCD Platform consumes.
    
    Signed-off-by: thiyagukb <thiyagux.kesavan.balakrishnan@intel.com>

---
+++ OvmfPkg/QemuUniversalPayload/QemuPayloadPcdListLibNull/QemuPayloadPcdListLibNull.c
@@ -0,0 +1,21 @@
+/** @file
+  Library instance to list all DynamicEx PCD PPL consumes.
+  No real functionality
+
+  Copyright (c) 2021 , Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+#include <Base.h>
+
+/**
+  Do nothing function.
+
+**/
+VOID
+QemuPayloadPcdListLibNull (
+  VOID
+  )
+{
+  return;
+}
